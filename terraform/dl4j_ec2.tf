bc24e51e1ba2ffae1403c29cb30254d6d380f238
ponkotuy <web@ponkotuy.com>
2018-09-30 16:43:30 +0900

    Fix security group

--- terraform/dl4j_ec2.tf
+++ terraform/dl4j_ec2.tf
@@ -5,4 +5,21 @@ resource "aws_instance" "dl4j" {
   tags {
     Name = "dl4j"
   }
+  vpc_security_group_ids = [
+    "${var.default_security_group}",
+    "${aws_security_group.ssh_allow.id}"
+  ]
+  key_name = "${var.ec2_key_name}"
+}
+
+resource "aws_security_group" "ssh_allow" {
+  name = "ssh_allow"
+  description = "Allow SSH Access"
+  vpc_id = "${var.vpc_id}"
+  ingress {
+    from_port = 0
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["124.41.70.210/32"]
+  }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,15 @@ provider "aws" {
   region = "us-west-2"
   version = "= 1.38.0"
 }
+
+variable "vpc_id" {
+  default = "vpc-3800105a"
+}
+
+variable "ec2_key_name" {
+  default = "ec2"
+}
+
+variable "default_security_group" {
+  default = "sg-73372c11"
+}
