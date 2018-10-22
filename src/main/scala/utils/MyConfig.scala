3f08ed7e9751a81312a2dbc8d8565365d666e34f
ponkotuy <web@ponkotuy.com>
2018-10-23 01:48:49 +0900

    Fix MyConfig to lazy val

--- src/main/scala/utils/MyConfig.scala
+++ src/main/scala/utils/MyConfig.scala
@@ -7,31 +7,29 @@ import com.typesafe.config.{Config, ConfigFactory}
 object MyConfig {
   private[this] val config = ConfigFactory.load()
 
-  val path = new Path(config.getConfig("path"))
-
-  val property = new Property(config.getConfig("property"))
-
-  val aws = new Aws(config.getConfig("aws"))
+  lazy val path = new Path(config.getConfig("path"))
+  lazy val property = new Property(config.getConfig("property"))
+  lazy val aws = new Aws(config.getConfig("aws"))
 }
 
 class Path(config: Config) {
-  val modelName = config.getString("model_file")
-  val dl4jPath: JPath = Paths.get(config.getString("dl4j_dir"))
-  val imagesPath: JPath = dl4jPath.resolve(config.getString("images_dir"))
-  val modelPath: JPath = dl4jPath.resolve(modelName)
-  val imageRsync: String = config.getString("image_rsync")
-  val originalImagePath: JPath = Paths.get(config.getString("original_images_dir"))
+  lazy val modelName = config.getString("model_file")
+  lazy val dl4jPath: JPath = Paths.get(config.getString("dl4j_dir"))
+  lazy val imagesPath: JPath = dl4jPath.resolve(config.getString("images_dir"))
+  lazy val modelPath: JPath = dl4jPath.resolve(modelName)
+  lazy val imageRsync: String = config.getString("image_rsync")
+  lazy val originalImagePath: JPath = Paths.get(config.getString("original_images_dir"))
 }
 
 class Property(config: Config) {
-  val width = config.getInt("image.width")
-  val height = config.getInt("image.height")
-  val epoch = config.getInt("epoch")
+  lazy val width = config.getInt("image.width")
+  lazy val height = config.getInt("image.height")
+  lazy val epoch = config.getInt("epoch")
 }
 
 class Aws(config: Config) {
-  val instanceId = config.getString("ec2.instance_id")
+  lazy val instanceId = config.getString("ec2.instance_id")
   def instance = new EC2Instance(instanceId)
-  val bucketName = config.getString("s3.bucket")
-  val bucket = new S3Bucket(bucketName)
+  lazy val bucketName = config.getString("s3.bucket")
+  lazy val bucket = new S3Bucket(bucketName)
 }
